bf4e120fdc11320f768d953917f07e6317c336a0
Dazh18 <azocardavid26@gmail.com>
2024-02-20 18:25:37 -0300

    Actualizacion

--- Proyecto tetris/te.js
+++ Proyecto tetris/te.js
@@ -1,12 +1,20 @@
-console.log("hello word")
 document.addEventListener('DOMContentLoaded', () =>  {
 
+const starBtn = document.querySelector('button')
 const grid = document.querySelector('.grid');
+const scoreDisplay = document.querySelector('.score-display');
+const linesDisplay = document.querySelector('.lines-display');
+const displaySquares = document.querySelectorAll('.previous-grid div')
 let squares = Array.from(grid.querySelectorAll('div'));
 const width = 10;
 const height = 20;
 let currentPosition = 4;
+let currentIndex = 0;
+let timerId 
+let score = 0
+let lines = 0 
 
+/* Controles flechitas */
   function control(e){
     if(e.keyCode === 39){
         moveRight()
@@ -22,7 +30,7 @@ let currentPosition = 4;
   document.addEventListener('keyup', control);
 
 
-//The tetrominoes
+//The tetrominoes piezas
   const  lTetrominoes = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
@@ -88,7 +96,7 @@ function draw() {
   
   function moveDown() {
     undraw()
-    currentPosition += height; // Use the height constant instead of width
+    currentPosition = currentPosition+= width; // Use the height constant instead of width
     draw()
     freeze()
   }
@@ -106,8 +114,8 @@ function draw() {
 
   function moveLeft() {
     undraw()
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width ===0)
-    if(isAtLeftEdge) currentPosition -=1
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    if(!isAtLeftEdge) currentPosition -=1
     if (current.some(index => squares[currentPosition + index].classList.contains('block2'))){
         currentPosition +=1
     }
@@ -115,7 +123,7 @@ function draw() {
   }
 
 // rotate
-  function rotate(){
+ function rotate(){
     undraw()
     currentRotation ++
     if(currentRotation === current.length){
@@ -124,15 +132,92 @@ function draw() {
     current = theTetrominoes[random][currentRotation]
     draw()
   };
+  /* detener en parte baja */
+ function freeze(){
+  if (current.some(index => squares[currentPosition + index + width].classList.contains('block3') 
+  || squares[currentPosition + index + width].classList.contains('block2'))) {
+    // make it block2
+    current.forEach(index => squares[index + currentPosition].classList.add('block2'))
+    random = nextRandom
+    nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+    displayShape() 
+    addScore()
+    gameOver()
+ }}
 
+
+
+ starBtn.addEventListener('click', () => {
+  if(timerId){
+    clearInterval(timerId)
+    timerId= null
+  } else {
+    draw()
+    timerId = setInterval(moveDown, 1000)
+    nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+    displayShape()
+    gameOver()
+    addScore()
+  }
+ })
 
   const displayWidth = 4;
   const displayIndex = 0;
 
+  function gameOver(){
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))){
+        scoreDisplay.innerHTML = 'end'
+        clearInterval(timerId)
+    }
+  }
+  /* visor pequeño de la pieza que sigue a continuacion */
   const smallTetrominoes = [
-    [1, displayWidth + 1]
+    [1, displayWidth+1, displayWidth*2+1, 2], /* l Tetromino*/
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], /* z Tetromino*/
+    [1, displayWidth, displayWidth+1, displayWidth+2], /* t Tetromino*/
+    [0, 1, displayWidth, displayWidth+1], /* o Tretomino */
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] /* i Tetromino */
   ];
 
+  function displayShape () {
+    displaySquares.forEach(square => {
+      square.classList.remove('block')
+    });
+    nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+    smallTetrominoes[nextRandom].forEach (
+      index => {
+        displaySquares[displayIndex + index].classList.add('block')
+      })
+  }
+  /*Elimina lineas completas, suma puntos y cantidad de lineas completadas*/
+  function addScore(){
+    for (currentIndex = 0; currentIndex < 199; currentIndex += width){
+      const row = [ currentIndex, currentIndex+1, currentIndex+2, currentIndex+3, currentIndex+4, 
+      currentIndex+5, currentIndex+6, currentIndex+7, currentIndex+8, currentIndex+9]
 
+      if (row.every(index => squares[index].classList.contains('block2'))) {
+        score += 10;
+        lines += 1;
+        scoreDisplay.innerHTML = score
+        linesDisplay.innerHTML = lines
+        row.forEach(index => {
+          squares[index].classList.remove('block2');
+          squares[index].classList.remove('block');
+        });
+        
+
+        const squaresRemoved = squares.splice(currentIndex, width)
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
+      }
+    }
+  }
+  
+
+  
+displayShape()
 
 });
